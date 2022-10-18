961e7e823e559f71f8d093a4804f5d6803f2de39
Matias Nabarro <matiasnabarro@gmail.com>
2022-10-18 16:06:38 +0200

    refactor: moved fileListToCompanyList method to a separated "converter" class

--- XMLImporter/src/main/java/BatchXmlImporter.java
+++ XMLImporter/src/main/java/BatchXmlImporter.java
@@ -1,11 +1,9 @@
+import converters.FileListToCompanyList;
 import infraestructure.LocalFileSystem;
-import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Unmarshaller;
 import xmlmodels.Company;
 import xmlmodels.Staff;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.sql.*;
@@ -19,7 +17,7 @@ public class BatchXmlImporter {
 
         ArrayList<Company> companies = new ArrayList<>();
 
-        fileListToCompanyList(paths, companies);
+        FileListToCompanyList.convert(paths, companies);
 
         processCompanies(companies);
     }
@@ -84,16 +82,6 @@ public class BatchXmlImporter {
             preparedStatement.setString(2, staff.salary.currency);
             preparedStatement.setInt(3, staff.salary.value);
             preparedStatement.executeUpdate();
-        }
-    }
-
-    private static void fileListToCompanyList(List<Path> paths, ArrayList<Company> companies) throws JAXBException {
-        for (Path path : paths) {
-            File file = new File(path.toString());
-            JAXBContext jaxbContext = JAXBContext.newInstance(Company.class);
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            Company company = (Company) jaxbUnmarshaller.unmarshal(file);
-            companies.add(company);
         }
     }
 
