199fb7d53144d6be0ba50631b2598d3f796f5062
Matias Nabarro <matiasnabarro@gmail.com>
2022-10-26 15:56:12 +0200

    refactor: deleted CompanyList class, was not necessary to keep that list on a separated class.

--- XMLImporter/src/main/java/application/CompanyList.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-import java.util.ArrayList;
-import xmlmodels.Company;
-
-public class CompanyList {
-
-  public static final ArrayList<Company> companies = new ArrayList<>();
-}
